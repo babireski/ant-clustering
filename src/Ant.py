c52354eefc0e77c6532e8d40cdaba86f02cc8e0c
Elian Babireski <55952848+babireski@users.noreply.github.com>
2024-03-27 10:16:07 -0300

    Probability fixed

--- src/Ant.py
+++ src/Ant.py
@@ -12,30 +12,35 @@ class Ant:
 
     def work(self, grid):
         self.drop(grid) if self.state else self.pick(grid)
-        neighbours = [place for place in self.neighbours(grid) if place not in map(lambda ant: ant.place, grid.ants)]
+        neighbours = [place for place in self.neighbours(grid, 1) if place not in map(lambda ant: ant.place, grid.ants)]
         if len(neighbours) > 0:
             self.place = random.choice(neighbours)
 
     def pick(self, grid):
         if self.place in grid.data:
-            similarity = self.similarity(grid)
-            probability = (self.k[0] / (self.k[0]) + similarity) ** 2
-            if not random.uniform(0, 1) > probability:
+            neighbours = len(self.neighbours(grid, self.sight, data = True))
+            vision = (self.sight * 2 + 1) ** 2 - 1
+            probability = 1 - (neighbours / vision) ** 2
+            if random.uniform(0, 1) < probability:
                 self.state = True
                 grid.data.remove(self.place)
 
     def drop(self, grid):
         if self.place not in grid.data:
-            similarity = self.similarity(grid)
-            probability = (similarity / (self.k[1] + similarity)) ** 2
-            if not random.uniform(0, 1) > probability:
-                self.state = True
+            neighbours = len(self.neighbours(grid, self.sight, data = True))
+            vision = (self.sight * 2 + 1) ** 2 - 1
+            probability = (neighbours / vision) ** 2
+            if random.uniform(0, 1) < probability:
+                self.state = False
                 grid.data.append(self.place)
 
-    def neighbours(self, grid):
+    def neighbours(self, grid, sight, data = False):
         x, y = self.place
-        return [((x + 1) % grid.size, (y + 1) % grid.size), ((x + 1) % grid.size, y), ((x + 1) % grid.size, (y - 1 + grid.size) % grid.size), (x, (y + 1) % grid.size), (x, y - 1), ((x - 1 + grid.size) % grid.size, (y + 1) % grid.size), ((x - 1 + grid.size) % grid.size, y), ((x - 1 + grid.size) % grid.size, (y - 1 + grid.size) % grid.size)]
-    
+        neighbours = [((x + 1) % grid.size, (y + 1) % grid.size), ((x + 1) % grid.size, y), ((x + 1) % grid.size, (y - 1 + grid.size) % grid.size), (x, (y + 1) % grid.size), (x, y - 1), ((x - 1 + grid.size) % grid.size, (y + 1) % grid.size), ((x - 1 + grid.size) % grid.size, y), ((x - 1 + grid.size) % grid.size, (y - 1 + grid.size) % grid.size)]
+        if data:
+            return list(set(grid.data).intersection(neighbours))
+        return neighbours
+
     def similarity(self, grid):
         neighbours = len(list(set(grid.data).intersection(self.neighbours(grid))))
         return 1 / neighbours ** 2 if neighbours > 0 else 0
